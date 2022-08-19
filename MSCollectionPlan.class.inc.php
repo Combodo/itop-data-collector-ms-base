f6652a3eb1e9197eecba7b6c8a004c536c12dde0
xtophe38 <christophe.naud@gmail.com>
2022-08-19 16:27:06 +0200

    Change $aMSObjectsToConsider structure

--- MSCollectionPlan.class.inc.php
+++ MSCollectionPlan.class.inc.php
@@ -22,7 +22,7 @@ class MSCollectionPlan
 	}
 
 	/**
-	 * Tells if a collector need to be orchestrated or not
+	 * Tells if a collector needs to be orchestrated or not
 	 *
 	 * @param $sCollectorClass
 	 *
@@ -37,26 +37,45 @@ class MSCollectionPlan
 	/**
 	 * Enrich the list of objects that can be considered during collections
 	 *
-	 * @param $sObjectL1 : URI_PARAM_SUBSCRIPTION or URI_PARAM_GROUP
-	 * @param $sObjectL2 : URI_PARAM_RESOURCEGROUP
-	 * @param $sObjectL3 : URI_PARAM_SERVER ou URI_PARAM_VNET
+	 * @param $aObjL1 : 1st level object : ex URI_PARAM_SUBSCRIPTION or URI_PARAM_GROUP
+	 * @param $aObjL2 : 2nd level object : ex URI_PARAM_RESOURCEGROUP
+	 * @param $aObjL3 : 3rd level object : ex URI_PARAM_SERVER
+	 * Each $aObjLi = array('class', 'id')
 	 *
 	 * @return void
 	 */
-	public function AddMSObjectsToConsider($sObjectL1, $sObjectL2, $sObjectL3)
+	public function AddMSObjectsToConsider($aObjL1, $aObjL2, $aObjL3)
 	{
-		if ($sObjectL1 != null) {
-			if (!array_key_exists($sObjectL1, $this->aMSObjectsToConsider)) {
-				$this->aMSObjectsToConsider[$sObjectL1] = [];
+		// Process 1st level first
+		if (!empty($aObjL1)) {
+			if (!array_key_exists($aObjL1['class'], $this->aMSObjectsToConsider)) {
+				$this->aMSObjectsToConsider[$aObjL1['class']] = [];
+				$this->aMSObjectsToConsider[$aObjL1['class']][$aObjL1['id']] = [];
+			} elseif (!array_key_exists($aObjL1['id'], $this->aMSObjectsToConsider[$aObjL1['class']])) {
+				$this->aMSObjectsToConsider[$aObjL1['class']][$aObjL1['id']] = [];
 			}
-			if ($sObjectL2 != null) {
-				if (!array_key_exists($sObjectL2, $this->aMSObjectsToConsider[$sObjectL1])) {
-					$this->aMSObjectsToConsider[$sObjectL1][$sObjectL2] = [];
+
+			// Next process 2nd level if required
+			if (!empty($aObjL2)) {
+				$aL1ToConsider = $this->aMSObjectsToConsider[$aObjL1['class']][$aObjL1['id']];
+				if (!array_key_exists($aObjL2['class'], $aL1ToConsider)) {
+					$aL1ToConsider[$aObjL2['class']] = [];
+					$aL1ToConsider[$aObjL2['class']][$aObjL2['id']] = [];
+				} elseif (!array_key_exists($aObjL2['id'], $aL1ToConsider[$aObjL2['class']])) {
+					$aL1ToConsider[$aObjL2['class']][$aObjL2['id']] = [];
 				}
-				if ($sObjectL3 != null) {
-					if (!array_key_exists($sObjectL3, $this->aMSObjectsToConsider[$sObjectL1][$sObjectL2])) {
-						$this->aMSObjectsToConsider[$sObjectL1][$sObjectL2][$sObjectL3] = [];
+				$this->aMSObjectsToConsider[$aObjL1['class']][$aObjL1['id']] = $aL1ToConsider;
+
+				// Then process 3rd level if required
+				if (!empty($aObjL3)) {
+					$aL2ToConsider = $aL1ToConsider[$aObjL2['class']][$aObjL2['id']];
+					if (!array_key_exists($aObjL3['class'], $aL2ToConsider)) {
+						$aL2ToConsider[$aObjL3['class']] = [];
+						$aL2ToConsider[$aObjL3['class']][$aObjL3['id']] = [];
+					} elseif (!array_key_exists($aObjL3['id'], $aL2ToConsider[$aObjL3['class']])) {
+						$aL2ToConsider[$aObjL3['class']][$aObjL3['id']] = [];
 					}
+					$this->aMSObjectsToConsider[$aObjL1['class']][$aObjL1['id']][$aObjL2['class']][$aObjL2['id']] = $aL2ToConsider;
 				}
 			}
 		}
@@ -74,13 +93,17 @@ class MSCollectionPlan
 	}
 
 	/**
-	 * Is there any subscription to consider in the  collection plan ?
+	 * Is there any subscription to consider in the collection plan ?
 	 *
 	 * @return bool
 	 */
 	protected function IsSubscriptionToConsider(): bool
 	{
-		return (empty($this->aMSObjectsToConsider) ? false : true);
+		if (array_key_exists(MSJsonCollector::URI_PARAM_SUBSCRIPTION, $this->aMSObjectsToConsider) && !empty($this->aMSObjectsToConsider[MSJsonCollector::URI_PARAM_SUBSCRIPTION])) {
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
@@ -90,9 +113,12 @@ class MSCollectionPlan
 	 */
 	protected function IsResourceGroupToConsider(): bool
 	{
-		foreach ($this->aMSObjectsToConsider as $sSubscription => $aResourceGroup) {
-			if (!empty($aResourceGroup)) {
-				return true;
+		if ($this->IsSubscriptionToConsider()) {
+			$aSubscriptions = $this->aMSObjectsToConsider[MSJsonCollector::URI_PARAM_SUBSCRIPTION];
+			foreach ($aSubscriptions as $aSubscription) {
+				if (array_key_exists(MSJsonCollector::URI_PARAM_RESOURCEGROUP, $aSubscription) && !empty($aSubscription[MSJsonCollector::URI_PARAM_RESOURCEGROUP])) {
+					return true;
+				}
 			}
 		}
 
